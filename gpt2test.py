b0c2a8abf66ed45595447c4177d60b1fb814bbd1
Johann Pérez <johannsw@gmail.com>
2020-01-29 12:56:52 -0300

    save results to file

--- gpt2test.py
+++ gpt2test.py
@@ -4,4 +4,8 @@ gpt2.download_gpt2()
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess, 'sampletext.txt', steps=1000)
 
-gpt2.generate(sess)
+sampletext = gpt2.generate(sess)
+
+f = open('gpt2generated.txt', 'w')
+f.write(sampletext)
+f.close()
